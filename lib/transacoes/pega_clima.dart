44aef1394c9bda0f2e4287acec615d84fbfbc0ba
Hélio Herculano <66783101+HelioHerculano@users.noreply.github.com>
2022-02-11 09:28:52 -0800

    Update pega_clima.dart

--- lib/transacoes/pega_clima.dart
+++ lib/transacoes/pega_clima.dart
@@ -4,8 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<Map> pegaClima(String appId, String cidade) async {
-  String apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=$cidade&appid='
-      '${appId}&units=metric';
+  String apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=$cidade&appid=${appId}&units=metric';
   http.Response response = await http.get(Uri.parse(apiUrl));
 
   if (response.statusCode == 200) {
