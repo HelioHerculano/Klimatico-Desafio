e1a7ec1cfcd32cdb8b154c283e681cc0520654b5
Helio Herculano Mocumbi <heliomucumbi@gmail.com>
2022-02-11 05:37:57 -0800

    Aplicativo de temperatura

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'ui/klimatico.dart';
+
+void main() => runApp(
+    new MaterialApp(
+      home: Klimatico(),
+    )
+);
---
+++ lib/util/util.dart
@@ -0,0 +1,2 @@
+final appId = "e2c69c0398ac36e92587aeb1e75d399d";
+final minhaCidade = "Maputo";
